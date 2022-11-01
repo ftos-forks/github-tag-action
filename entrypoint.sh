9e66be4ab1947e2da00dede2be221b705f9548bc
Santiago Bernhardt <santiago.bernhardt@gmail.com>
2022-11-02 09:17:32 +1300

    go back to simple and use git tag git push

--- entrypoint.sh
+++ entrypoint.sh
@@ -197,35 +197,8 @@ then
     exit 0
 fi
 
-# create local git tag
-git tag "$new"
-
-# push new tag ref to github
-dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
-full_name=$GITHUB_REPOSITORY
-git_refs_url=$(jq .repository.git_refs_url "$GITHUB_EVENT_PATH" | tr -d '"' | sed 's/{\/sha}//g')
-
 echo "$dt: **pushing tag $new to repo $full_name"
-
-git_refs_response=$(
-curl -s -X POST "$git_refs_url" \
--H "Authorization: token $GITHUB_TOKEN" \
--d @- << EOF
-
-{
-  "ref": "refs/tags/$new",
-  "sha": "$commit"
-}
-EOF
-)
-
-git_ref_posted=$( echo "${git_refs_response}" | jq .ref | tr -d '"' )
-
-echo "::debug::${git_refs_response}"
-if [ "${git_ref_posted}" = "refs/tags/${new}" ]
-then
-    exit 0
-else
-    echo "::error::Tag was not created properly."
-    exit 1
-fi
+# create local git tag
+git tag -f "$new"
+# push local tag
+git push -f origin "$new"
