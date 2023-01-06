22568f5e0aa11c2da74399711416867449bb59f4
Santiago Bernhardt <santiago.bernhardt@gmail.com>
2023-01-06 22:55:35 +1300

    another alternative without rev

--- entrypoint.sh
+++ entrypoint.sh
@@ -127,7 +127,7 @@ fi
 
 # sanitize that the default_branch is set (via env var when running on PRs) else find it natively
     echo "The DEFAULT_BRANCH should be autodetected when tag-action runs on on PRs else must be defined, See: https://github.com/anothrNick/github-tag-action/pull/230, since is not defined we find it natively"
-    default_branch=$(git branch -rl '*/HEAD' | rev | cut -d/ -f1 | rev)
+    default_branch=$(git branch -r --points-at refs/remotes/origin/HEAD | grep '\->' | cut -d' ' -f5 | cut -d/ -f2)
     echo "default_branch=${default_branch}"
     # re check this
     if [ -z "${default_branch}" ]
